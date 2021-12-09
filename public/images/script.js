ecaa64fff607233258e95cd6636cced856b5b450
Brennan Butler <developers@mesmerise.media>
2021-12-09 23:06:42 +0000

    Updated UI

---
+++ public/images/script.js
@@ -0,0 +1,368 @@
+// change the host if required, important to allow code to run. This is used because base.html configures the heading for every page. 
+
+const host= "http://127.0.0.1/";
+
+
+// code for sound toggle 
+
+// adds tool tip to sound icon 
+
+$(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip({
+        placement: 'bottom'
+    });
+    
+  });
+
+
+
+if (window.location.href == host | window.location.href == host + 'yourgarden'){
+
+    var image = document.getElementById("sound");
+    image.src = "static/soundOn.png";
+    var sound = document.getElementById("audio");
+  
+
+    function togglePlay() {
+        if (sound.paused) {
+            image.src= "static/soundOn.png";
+            sound.play();
+        }
+        else {
+            sound.pause();
+            image.src="static/soundOff.png";
+        }
+    };
+
+    image.addEventListener("click",togglePlay, false);   
+
+}
+
+
+// code for question 2 
+
+if (window.location.href == host + 'fluency2')
+
+{
+
+    var video = document.getElementById("video");
+    var videoDiv = document.getElementById("videoDiv")
+    video.style.display = "none";
+    var questionBlock= document.getElementById("questions")
+    questionBlock.style.display = "none";
+
+    function play() {
+        video.play();
+        document.getElementById("play").style.display = "none";
+        document.getElementById("videoDiv").style.display = "none";
+    }
+
+    function showQuestions(){
+
+        questionBlock.style.display = "block";
+    }
+
+
+    function questions() {
+        play();
+        setTimeout(showQuestions, 17500);
+        
+    }
+
+
+    playButton= document.getElementById("play");
+    playButton.addEventListener('click', questions, false);
+
+}
+
+// code for question 6 
+
+if (window.location.href == host + 'fluency6')
+
+{
+    var startDiv = document.getElementById("startDiv");
+    var soundDiv= document.getElementById("soundDiv");
+    var div1= document.getElementById("div1");
+    var div2= document.getElementById("div2");
+    var div3= document.getElementById("div3");
+    var div4= document.getElementById("div4");
+    var next = document.getElementById("next");
+
+    var start = document.getElementById("startBtn");
+   
+
+    soundDiv.style.display = "none";
+    div1.style.display = "none";
+    div2.style.display = "none";
+    div3.style.display = "none";
+    div4.style.display = "none";
+   
+
+    var audio1 = document.getElementById("audio1");
+    var audio2 = document.getElementById("audio2");
+    var audio3 = document.getElementById("audio3");
+    var audio4 = document.getElementById("audio4");
+
+    var flower1 = document.getElementById("flower1");
+    var flower2 = document.getElementById("flower2");
+    var flower3 = document.getElementById("flower3");
+    var flower4 = document.getElementById("flower4");
+
+    var can1 = document.getElementById("can1");
+    var can2 = document.getElementById("can2");
+    var can3 = document.getElementById("can3");
+    var can4 = document.getElementById("can4");
+
+    
+
+    var finish1 = document.getElementById("finish1");
+    var finish2 = document.getElementById("finish2");
+    var finish3 = document.getElementById("finish3");
+    var finish4 = document.getElementById("finish4");
+
+    var points = 0;
+
+    var clickedArray1 = []
+    var clickedArray2 = []
+    var clickedArray3 = []
+    var clickedArray4 = []
+
+
+    
+   
+
+    function playOne(){
+
+        startDiv.style.display = "none";
+        soundDiv.style.display ="block";
+        audio1.play()
+        
+
+
+    }
+
+    function showDiv1() {
+
+        soundDiv.style.display ="none"
+        div1.style.display = "block"
+
+    }
+
+
+    function begin() {
+
+        playOne()
+        setTimeout(showDiv1, 7000);
+    }
+
+    // code for step 1 
+
+    function clicked1(event){
+
+        if (event.target== flower1){
+
+            clickedArray1.push('f')
+        }
+
+        if (event.target == can1){
+
+            clickedArray1.push('c')
+        }
+        console.log(clickedArray1)
+    }
+
+ 
+
+    function evaluate1(){
+
+        if (JSON.stringify(clickedArray1) === JSON.stringify(['f','c'])) {
+
+            document.getElementById("v1").value = 1;
+            div1.style.display = "none";
+            soundDiv.style.display = "block";
+            audio2.play()
+        }
+        else{
+
+            document.getElementById("v1").value = 0;
+            document.getElementById("v2").value = 0;
+            document.getElementById("v3").value = 0;
+            document.getElementById("v4").value = 0;
+            document.getElementById("form").submit();
+            
+        }
+
+    }
+    
+    function showDiv2(){
+
+        soundDiv.style.display = "none";
+        div2.style.display = "block";
+    }
+
+    function question1(){
+        evaluate1()
+        setTimeout(showDiv2, 7000);
+    }
+
+
+
+ // code for step 2 
+
+    function clicked2(event){
+
+        if (event.target== flower2){
+
+            clickedArray2.push('f')
+        }
+
+        if (event.target == can2){
+
+            clickedArray2.push('c')
+        }
+        
+    }
+
+ 
+
+    function evaluate2(){
+
+        if (JSON.stringify(clickedArray2) === JSON.stringify(['c','f'])) {
+
+            document.getElementById("v2").value = 1;
+        
+        }
+        else{
+            document.getElementById("v2").value = 0;
+        }
+
+        div2.style.display = "none";
+        soundDiv.style.display = "block";
+        audio3.play()
+        
+
+    }
+    
+    function showDiv3(){
+
+        soundDiv.style.display = "none";
+        div3.style.display = "block";
+    }
+
+    function question2(){
+        evaluate2()
+        setTimeout(showDiv3, 6000);
+    }
+
+    // code for step 3
+    
+    function clicked3(event){
+
+        if (event.target== flower3){
+
+            clickedArray3.push('f')
+        }
+
+        if (event.target == can3){
+
+            clickedArray3.push('c')
+        }
+        
+    }
+
+ 
+
+    function evaluate3(){
+
+        if (JSON.stringify(clickedArray3) === JSON.stringify(['f'])) {
+
+            document.getElementById("v3").value = 1;
+        
+        }
+        else{
+            document.getElementById("v3").value = 0;
+        }
+
+        div3.style.display = "none";
+        soundDiv.style.display = "block";
+        audio4.play()
+        
+
+    }
+    
+    function showDiv4(){
+
+        soundDiv.style.display = "none";
+        div4.style.display = "block";
+    }
+
+    function question3(){
+        evaluate3()
+        setTimeout(showDiv4, 6000);
+    }
+
+
+    // code for step 4 
+
+    function clicked4(event){
+
+        if (event.target== flower4){
+
+            clickedArray4.push('f')
+        }
+
+        if (event.target == can4){
+
+            clickedArray4.push('c')
+        }
+        
+    }
+
+ 
+
+    function evaluate4(){
+
+        if (JSON.stringify(clickedArray4) === JSON.stringify(['f','c'])) {
+
+            document.getElementById("v4").value = 1;
+        
+        }
+        else{
+            document.getElementById("v4").value = 0;
+        }
+
+        document.getElementById("form").submit();
+        
+
+    }
+    
+
+
+
+
+
+    start.addEventListener('click', begin, false);
+
+    flower1.addEventListener('click', clicked1, false);
+    can1.addEventListener('click', clicked1, false);
+
+    finish1.addEventListener('click', question1, false)
+
+    flower2.addEventListener('click', clicked2, false);
+    can2.addEventListener('click', clicked2, false);
+
+    finish2.addEventListener('click', question2, false)
+
+    flower3.addEventListener('click', clicked3, false);
+    can3.addEventListener('click', clicked3, false);
+
+    finish3.addEventListener('click', question3, false)
+    
+    flower4.addEventListener('click', clicked4, false);
+    can4.addEventListener('click', clicked4, false);
+
+    finish4.addEventListener('click', evaluate4, false)
+
+
+
+}
--- src/App.js
+++ src/App.js
@@ -2,23 +2,117 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const Navigation = () => {
+
+  return (
+    <nav class="navbar sticky-top navbar-expand-md navbar-light">
+      <div class="container-fluid">
+
+        <a class="navbar-brand mx-3" href="/">
+          <img src="/static/logo.jpg" alt="" width="120" height="80"/>
+        </a>
+
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+          <ul class="navbar-nav me-auto mb-4 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link navbar-font-size text-dark mx-3" aria-current="page" href="/">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link navbar-font-size text-dark mx-3" href="/about">About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link navbar-font-size text-dark mx-3" href="/support">Support</a>
+            </li>    
+          </ul>
+          
+          <ul class="navbar-nav ms-auto  mx-4 mb-lg-0">
+            <li>
+              <a class="nav-link navbar-font-size text-dark mx-3" href="/createaccount">Create Account <span class="sr-only"></span></a>
+            </li>
+            <li>
+              <a class="nav-link navbar-font-size text-dark mx-3" href="/login">Login</a>
+            </li>
+          </ul>  
+           
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="not-mobile">
+        This demo is only available for mobile devices
+      </div>
+      <div class="page-body">
+            <div class="navigation-wrapper">
+
+                <div class="navigation-toggle">
+                
+
+
+                    <nav class="navigation-panel" id="nav-panel">
+                        <ul>
+                            <li class="link-item active">
+                                <a href="#">Home</a>
+                            </li>
+                            <li class="link-item">
+                                <a href="login.html">Login</a>
+                            </li>
+                            <li class="link-item">
+                                <a href="#">Sign up</a>
+                            </li>
+                            <li class="link-item">
+                                <a href="#">Home</a>
+                            </li>
+                        </ul>
+                    </nav>
+
+                    <a class="hamburger-toggle" href="#nav-panel">
+                        <svg width="38" height="38" viewBox="0 0 38 38" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M32.4583 11.0833H5.54165C5.12172 11.0833 4.71899 10.9165 4.42206 10.6196C4.12513 10.3226 3.95831 9.91992 3.95831 9.49999C3.95831 9.08006 4.12513 8.67734 4.42206 8.3804C4.71899 8.08347 5.12172 7.91666 5.54165 7.91666H32.4583C32.8782 7.91666 33.281 8.08347 33.5779 8.3804C33.8748 8.67734 34.0416 9.08006 34.0416 9.49999C34.0416 9.91992 33.8748 10.3226 33.5779 10.6196C33.281 10.9165 32.8782 11.0833 32.4583 11.0833Z" fill="white"/>
+                            <path d="M32.4583 20.5833H5.54165C5.12172 20.5833 4.71899 20.4165 4.42206 20.1196C4.12513 19.8226 3.95831 19.4199 3.95831 19C3.95831 18.5801 4.12513 18.1773 4.42206 17.8804C4.71899 17.5835 5.12172 17.4167 5.54165 17.4167H32.4583C32.8782 17.4167 33.281 17.5835 33.5779 17.8804C33.8748 18.1773 34.0416 18.5801 34.0416 19C34.0416 19.4199 33.8748 19.8226 33.5779 20.1196C33.281 20.4165 32.8782 20.5833 32.4583 20.5833Z" fill="white"/>
+                            <path d="M32.4583 30.0833H5.54165C5.12172 30.0833 4.71899 29.9165 4.42206 29.6196C4.12513 29.3226 3.95831 28.9199 3.95831 28.5C3.95831 28.0801 4.12513 27.6773 4.42206 27.3804C4.71899 27.0835 5.12172 26.9167 5.54165 26.9167H32.4583C32.8782 26.9167 33.281 27.0835 33.5779 27.3804C33.8748 27.6773 34.0416 28.0801 34.0416 28.5C34.0416 28.9199 33.8748 29.3226 33.5779 29.6196C33.281 29.9165 32.8782 30.0833 32.4583 30.0833Z" fill="white"/>
+                            </svg>
+                            
+                        </a>
+                </div>
+            </div>
+
+      
+            <header class="page-header">
+
+                <h1 class="page-title">The Mind Garden</h1>
+    
+            </header>
+            
+            <img class="image-overflow-center" src="images/mind-garden-intro.svg" alt="mind-garden-intro"/>
+
+
+            <div class="button-group align-bottom fw-m">
+
+                <button class="button-primary">
+                    Sign up
+                </button>
+
+                <div class="alternative-option">
+                    or
+                </div>
+
+                <button class="button-alternative">
+                    Log in
+                </button>
+            </div>
+      </div>
+     
     </div>
   );
 }
